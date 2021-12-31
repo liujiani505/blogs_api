a528992e359b3c09bf8140a6136166357d806d9e
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-12-31 18:20:10 -0500

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :update, :destroy]
-  before_action :authorized
+  # before_action :authorized
   # GET /comments
   def index
     @comments = Comment.where(user_id: @user.id)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
-  before_action :authorized
+  # before_action :authorized
   # GET /posts
   def index
     @posts = Post.where(user_id: @user.id)
