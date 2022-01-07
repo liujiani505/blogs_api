beb181744482fb05569c3f54ab9dd480ac9fe363
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2022-01-07 08:08:52 -0500

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   # delegate :username, :to => :user
+  def as_json(options={})
+    super(only: [:id, :description, :created_at, :user_id],
+    include: [user: { only: [:username] }]
+    )
+  end
 end
